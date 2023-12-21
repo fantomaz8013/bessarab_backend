bd9f310096400e8e9b1c3dd83d4820c8842f07fa
Andrey <fantomaz8013@mail.ru>
2023-12-21 19:24:04 +0300

    qwe

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,12 +15,14 @@ class OrderController extends Controller
 {
     /**
      * Список заказов
-     * @bodyParam firstName string . Example: Шампунь
-     * @bodyParam lastName string . Example: Шампунь
-     * @bodyParam email string . Example: Шампунь
-     * @bodyParam phone string . Example: Шампунь
-     * @bodyParam city string . Example: Шампунь
-     * @bodyParam address string . Example: Шампунь
+     * @bodyParam firstName string . Example: Иван
+     * @bodyParam lastName string . Example: Иванов
+     * @bodyParam email string . Example: Ivan@mail.ru
+     * @bodyParam phone string . Example: 89999999999
+     * @bodyParam city string . Example: Ивановск
+     * @bodyParam address string . Example: улица иванова 15 квартира 14
+     * @bodyParam page int Страница. Example: 1
+     * @bodyParam limit int Сколько выдать записей. Example: 10.
      */
     public function index(OrderFilter $filter)
     {
